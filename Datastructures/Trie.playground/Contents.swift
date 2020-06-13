4c6eb424ade87c80595fc3e6ad81a1b475c40876
Lamine Ndiaye <lamine.ndy@gmail.com>
2020-06-13 12:48:09 +0200

    Rewrite Tri implementation

--- Datastructures/Trie.playground/Contents.swift
+++ Datastructures/Trie.playground/Contents.swift
@@ -1,66 +1,84 @@
-import UIKit
-
-//var str = "Hello, playground"
+import Foundation
 
 final class TrieNode {
-  var c: Character
-  var children: [TrieNode]// With a maxime of 26 characteres correspoonding to the alphabetical
-  //var isWord: isWord
-  var size = 0
+  let char: Character
+  var children: [Character: TrieNode]
+  var isWord: Bool
   
-  init(c: Character, children: [TrieNode]) {
-    self.c = c
-    self.children = children
+  init(char: Character) {
+    self.char = char
+    self.children = [:]
+    self.isWord = false
   }
   
-  static func getCharIndex(c: Character) -> Int {
-    return c.al
-  }
-  
-  private func getNode(c: Character) -> TrieNode? {
-    self.children[TrieNode.getCharIndex(c: c)]
-  }
-  
-  private func setNode(c: Character, node: TrieNode) {
-    self.children[TrieNode.getCharIndex(c: c)] = node
-  }
-  
-  func add(s: String, index: Int) {
-    size += 1
-    if index == s.count { return }
-    let current = Array(s)[index]
-    let charIndex = TrieNode.getCharIndex(c: current)
-    var child = getNode(c: current)
-    if child == nil {
-      child = TrieNode(c: current, children: [])
-      child?.add(s: s, index: index + 1)
+  func addWord(_ word: String) {
+    if word.isEmpty {
+      return
+    }
+    // Get the first char
+    let arrayString = Array(word)
+    let firstChar = arrayString.first!
+    var firstChildNode = child(firstChar)
+    // Check if a node has been created
+    if firstChildNode == nil {
+      // if the node is missing we create one
+      firstChildNode = TrieNode(char: firstChar)
+      children[firstChar] = firstChildNode
+    }
+    // if it's the last char of the word we tag it as end of word
+    // else just recurse to as more children
+    if (word.count > 1) {
+      firstChildNode?.addWord(String(word.dropFirst()))
+    } else {
+      firstChildNode?.isWord = true
     }
   }
   
-  func findCount(s: String, index: Int) -> Int {
-    if s.count == index {
-      return size
-    }
-    
-    var child = getNode(c: Array(s)[index])
-    if child == nil {
-      return 0
-    }
-    
-    findCount(s: s, index: index + 1)
+  func child(_ c:Character) -> TrieNode? {
+    return children[c]
   }
-  
-  // If a word has children -> skip
-  // if it's not a word, skp cause cannot delete a word not in the Tri
-  func deleteWord(s: String) {
-    
-  }
-  
 }
 
-public struct Vertex<T> : Equatable where T: Equatable, T: Hashable {
-
-  public var data: T
-  public let index: Int
-
+final class Trie {
+  var root: TrieNode
+  
+  init(wordList: [String]) {
+    root = TrieNode(char: "*")
+    for w in wordList {
+      root.addWord(w)
+    }
+  }
+  
+  func contains(prefix: String) -> Bool {
+    return contains(prefix: prefix, exactWord: false)
+  }
+  
+  private func contains(prefix: String, exactWord: Bool) -> Bool {
+    /*
+     * loop over the chars of the word and get the corresponding node for each of them
+     * if the node happen to be nil, thats means the word is mising some part
+     * if we get through the end, all the chars are found, we just need to check if it's a word
+     */
+    var lastNode: TrieNode? = root
+    for char in Array(prefix) {
+      lastNode = lastNode?.child(char)
+      if lastNode == nil {
+        return false
+      }
+    }
+    
+    return !exactWord || lastNode!.isWord
+  }
 }
+
+var trie = Trie(wordList: [
+  "cracking", "racking", "king", "something", "some"
+])
+
+trie.contains(prefix: "some") == true
+trie.contains(prefix: "notHereAtAll") == false
+trie.contains(prefix: "somed") == false
+trie.contains(prefix: "king") == true
+
+
+
